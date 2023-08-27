9039f98a1e922e585a246213cb9686b0f8c2bd4e
samanseifi <samansei@bu.edu>
2023-08-27 16:08:31 -0400

    #7 adding error logging and fixing error handling

--- scraper/poll_data_processor.py
+++ scraper/poll_data_processor.py
@@ -3,32 +3,63 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
+import logging
 
 class PollDataProcessor:
     def __init__(self, dataframe):
         self.dataframe = dataframe
+        self.logger = self.setup_logger()  # Initialize the logger
+    
+    def setup_logger(self):
+        # Set up logging configuration
+        logger = logging.getLogger("poll_data_processor")
+        logger.setLevel(logging.ERROR)
+        
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        
+        file_handler = logging.FileHandler('processor_error_log.txt')  # Log errors to a file
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+        
+        return logger
 
     def preprocess_data(self, candidates):
         processed_data = {}
-        for candidate in candidates:
-            candidate_data = self.dataframe[['date', candidate]].dropna()
-            x = np.array((candidate_data['date'] - candidate_data['date'].min()).dt.days).reshape(-1, 1)
-            y = np.array(candidate_data[candidate])
-            processed_data[candidate] = (x, y)
+        try:
+            for candidate in candidates:
+                candidate_data = self.dataframe[['date', candidate]].dropna()
+                x = np.array((candidate_data['date'] - candidate_data['date'].min()).dt.days).reshape(-1, 1)
+                y = np.array(candidate_data[candidate])
+                processed_data[candidate] = (x, y)
+        except Exception as e:
+            self.logger.error(f"An error occurred while preprocessing data: {e}")  # Log the error
+            raise e
         return processed_data
 
     def fit_gaussian_process(self, x, y):
-        kernel = C(1.0, (1e-3, 1e3)) * RBF(1.0, (1e-2, 1e2))
-        model = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=10, alpha=0.001)
-        model.fit(x, y)
-        return model
+        try:
+            kernel = C(1.0, (1e-3, 1e3)) * RBF(1.0, (1e-2, 1e2))
+            model = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=10, alpha=0.001)
+            model.fit(x, y)
+            return model
+        except Exception as e:
+            self.logger.error(f"An error occurred while fitting Gaussian Process: {e}")  # Log the error
+            raise e
 
     def predict_gaussian_process(self, model, x):
-        y_pred, sigma = model.predict(x, return_std=True)
-        return y_pred, sigma
+        try:
+            y_pred, sigma = model.predict(x, return_std=True)
+            return y_pred, sigma
+        except Exception as e:
+            self.logger.error(f"An error occurred while predicting using Gaussian Process: {e}")  # Log the error
+            raise e
 
     def calculate_moving_average(self, data_series, window_size=7):
-        return data_series.rolling(window=window_size, min_periods=1).mean()
+        try:
+            return data_series.rolling(window=window_size, min_periods=1).mean()
+        except Exception as e:
+            self.logger.error(f"An error occurred while calculating moving average: {e}")  # Log the error
+            raise e
 
     def plot_polling_data(self, x, y_actual, y_pred, sigma, label, method, color):
         plt.plot(x, y_actual, marker='o', linestyle='', alpha=0.3, label=label, color=color)
@@ -41,38 +72,45 @@ class PollDataProcessor:
         processed_data = self.preprocess_data(candidates)
 
         plt.figure(figsize=(10, 6))
-        for i, candidate in enumerate(candidates):
-            x, y = processed_data[candidate]
-            model = self.fit_gaussian_process(x, y)
-            y_pred, sigma = self.predict_gaussian_process(model, x)
-            self.plot_polling_data(x, y, y_pred, sigma, candidate, method="Gaussian Process", color=colors[i])
+        try:
+            for i, candidate in enumerate(candidates):
+                x, y = processed_data[candidate]
+                model = self.fit_gaussian_process(x, y)
+                y_pred, sigma = self.predict_gaussian_process(model, x)
+                self.plot_polling_data(x, y, y_pred, sigma, candidate, method="Gaussian Process", color=colors[i])
 
-
-        plt.xlabel('Date')
-        plt.ylabel('Percentage')
-        plt.title('Gaussian Process Regression Trends for Candidates')
-        plt.legend()
-        plt.grid(False)
-        plt.xticks(rotation=45)
-        plt.tight_layout()
-        plt.show()
+            plt.xlabel('Date')
+            plt.ylabel('Percentage')
+            plt.title('Gaussian Process Regression Trends for Candidates')
+            plt.legend()
+            plt.grid(False)
+            plt.xticks(rotation=45)
+            plt.tight_layout()
+            plt.show()
+        except Exception as e:
+            self.logger.error(f"An error occurred while plotting Gaussian Process trends: {e}")  # Log the error
+            raise e
 
     def plot_moving_average_trends(self, candidates, window_size):
         colors = ['blue', 'green', 'red', 'purple', 'brown', 'orange']  # Choose your desired colors
 
         plt.figure(figsize=(10, 6))
-        for candidate in candidates:
-            candidate_data = self.dataframe[['date', candidate]].dropna()
-            candidate_data = candidate_data.set_index('date')
-            rolling_average = self.calculate_moving_average(candidate_data[candidate], window_size=window_size)
-            self.plot_polling_data(candidate_data.index, candidate_data[candidate], rolling_average, None, candidate, 'Moving Average', color=colors[candidates.index(candidate)])
+        try:
+            for candidate in candidates:
+                candidate_data = self.dataframe[['date', candidate]].dropna()
+                candidate_data = candidate_data.set_index('date')
+                rolling_average = self.calculate_moving_average(candidate_data[candidate], window_size=window_size)
+                self.plot_polling_data(candidate_data.index, candidate_data[candidate], rolling_average, None, candidate, 'Moving Average', color=colors[candidates.index(candidate)])
 
-        plt.xlabel('Date')
-        plt.ylabel('Percentage')
-        plt.title(str(window_size)+'-day Moving Average Trends for Candidates')
-        plt.legend()
-        plt.grid(False)
-        plt.xticks(rotation=45)
-        plt.tight_layout()
-        plt.show()
+            plt.xlabel('Date')
+            plt.ylabel('Percentage')
+            plt.title(str(window_size)+'-day Moving Average Trends for Candidates')
+            plt.legend()
+            plt.grid(False)
+            plt.xticks(rotation=45)
+            plt.tight_layout()
+            plt.show()
+        except Exception as e:
+            self.logger.error(f"An error occurred while plotting moving average trends: {e}")  # Log the error
+            raise e
 
--- scraper/poll_data_scraper.py
+++ scraper/poll_data_scraper.py
@@ -4,94 +4,99 @@ import csv
 import pandas as pd
 import re
 from typing import List
+import logging
 
 class PollDataScraper:
     def __init__(self, url: str):
         self.url = url
         self.soup = None
+        self.logger = self.setup_logger()
 
+    def setup_logger(self):
+        logger = logging.getLogger("poll_data_scraper")
+        logger.setLevel(logging.ERROR)
+        
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        
+        file_handler = logging.FileHandler('error_log.txt')
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+        
+        return logger
+    
     def fetch_page(self):
         try:
             response = requests.get(self.url)
             response.raise_for_status()  # Raise an exception for 4xx and 5xx status codes
             self.soup = BeautifulSoup(response.content, "html.parser")
         except requests.exceptions.RequestException as e:
+            self.logger.error(f"Error fetching the page: {e}")
             raise RuntimeError(f"Error fetching the page: {e}")
         
     def clean_cell(self, cell_text: str) -> str:
-        # Remove the * and ** if they appear at the end of the string
         cleaned_text = re.sub(r'[*+,]+', '', cell_text)
-
-        # This removes entries with only "**"
         cleaned_text = "" if cleaned_text == "**" else cleaned_text
-
-        # Convert percentage to decimal
         
         if "%" in cell_text:
             try:
                 cleaned_text = str(float(cleaned_text.strip('%')) / 100)
             except ValueError:
                 pass  # Handle the case where conversion to float fails
-
+        
         return cleaned_text
 
     def extract_column_candidates(self) -> List[str]:
         if self.soup is None:
             raise RuntimeError("Page content not fetched. Call fetch_page() first.")
-
+        
         table = self.soup.find("table")
-
-        # Find the table header
         thead = table.find("thead")
-
-        # Find all th elements within the thead
         column_names = [th.get_text(strip=True) for th in thead.find_all("th")]
-
-        # Remove the first three columns (Date, Pollster, Sample)
-        column_names = column_names[3:]
-
+        column_names = column_names[3:]  # Remove the first three columns
+        
         return column_names
     
     def clean_columns(self, df, column_names):
         for column_name in column_names:
-            # Convert the column to numeric
             df[column_name] = pd.to_numeric(df[column_name], errors='coerce')
-            
-            # Divide values between 1 and 100 by 100
             df.loc[(df[column_name] > 1) & (df[column_name] <= 100), column_name] /= 100
         
         return df
     
     def scrape_to_dataframe_and_csv(self, csv_filename: str) -> pd.DataFrame:
-        if self.soup is None:
-            raise RuntimeError("Page content not fetched. Call fetch_page() first.")
-        
-        table = self.soup.find("table")
-        if table is None:
-            raise ValueError("Table not found in the page content.")
-        
-        column_headers = ["date", "pollster", "n"] + self.extract_column_candidates()
-        data = []
-
-        tbody = table.find("tbody")
-        if tbody is None:
-            raise ValueError("Table body (tbody) not found in the table.")
-        
-        rows = tbody.find_all("tr")
-        if not rows:
-            raise ValueError("No rows found in the table.")
-
-        for row in rows:
-            cells = row.find_all("td")
-            if not cells:
-                print("Warning: Empty row found.")
-                continue
+        try:
+            if self.soup is None:
+                raise RuntimeError("Page content not fetched. Call fetch_page() first.")
             
-            row_data = [self.clean_cell(cell.get_text(strip=True)) for cell in cells]
-            data.append(row_data)
-        
-        df = pd.DataFrame(data, columns=column_headers)
-        df = self.clean_columns(df, column_headers[3:])
-        
-        df.to_csv(csv_filename, index=False)  # Save DataFrame to CSV
-        return df
+            table = self.soup.find("table")
+            if table is None:
+                raise ValueError("Table not found in the page content.")
+            
+            column_headers = ["date", "pollster", "n"] + self.extract_column_candidates()
+            data = []
+            
+            tbody = table.find("tbody")
+            if tbody is None:
+                raise ValueError("Table body (tbody) not found in the table.")
+            
+            rows = tbody.find_all("tr")
+            if not rows:
+                raise ValueError("No rows found in the table.")
+            
+            for row in rows:
+                cells = row.find_all("td")
+                if not cells:
+                    self.logger.warning("Empty row found.")
+                    continue
+                
+                row_data = [self.clean_cell(cell.get_text(strip=True)) for cell in cells]
+                data.append(row_data)
+            
+            df = pd.DataFrame(data, columns=column_headers)
+            df = self.clean_columns(df, column_headers[3:])
+            
+            df.to_csv(csv_filename, index=False)  # Save DataFrame to CSV
+            return df
+        except Exception as e:
+            self.logger.error(f"An error occurred: {e}")
+            raise e
