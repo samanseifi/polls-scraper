7d4dc6458637008393e69de2e8cc87a17f484a2f
samanseifi <samansei@bu.edu>
2023-08-27 11:55:14 -0400

    #4 adding unit test

--- tests/test_poll_data_scraper.py
+++ tests/test_poll_data_scraper.py
@@ -66,6 +66,13 @@ class TestPollDataScraper(unittest.TestCase):
     def test_clean_cell_number(self):
         cleaned_text = self.scraper.clean_cell("1,234*")
         self.assertEqual(cleaned_text, 1234)
+        
+        cleaned_text = self.scraper.clean_cell("1,235**")
+        self.assertEqual(cleaned_text, 1235)
+        
+    def test_clean_cell_stars(self):
+        cleaned_text = self.scraper.clean_cell("**")
+        self.assertEqual(cleaned_text, "")
 
     # Add more test cases for clean_cell based on your data variations
 
